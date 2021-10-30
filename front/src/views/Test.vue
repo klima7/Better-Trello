26ed106f4de0749317c4c9a0c753958872fc657d
klima7 <ukasz.klimkiewicz2@wp.pl>
2021-10-30 11:20:39 +0200

    Simple request

--- front/src/views/Test.vue
+++ front/src/views/Test.vue
@@ -3,3 +3,25 @@
     <h1>This is test page</h1>
   </div>
 </template>
+
+<script>
+import axios from 'axios';
+
+export default {
+  methods: {
+    performRequest() {
+      const path = 'https://pawtrello.herokuapp.com/';
+      axios.get(path)
+        .then((res) => {
+          alert(res.data)
+        })
+        .catch((error) => {
+          console.error(error);
+        });
+    },
+  },
+  created() {
+    this.performRequest()
+  }
+}
+</script>
