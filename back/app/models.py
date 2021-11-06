20f090ba7cb7823bec30db42a6ff20bf628d096a
klima7 <ukasz.klimkiewicz2@wp.pl>
2021-11-06 10:46:56 +0100

    get_todos route added

--- back/app/models.py
+++ back/app/models.py
@@ -1,6 +1,10 @@
 from app import db
+import json
 
 
 class Todo(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     content = db.Column(db.String)
+
+    def to_json(self):
+        return {"content": self.content}
--- back/app/routes.py
+++ back/app/routes.py
@@ -1,6 +1,7 @@
 from app import app
 from app.models import *
 from flask import request
+from flask import jsonify
 
 
 @app.route('/')
@@ -9,9 +10,16 @@ def index():
 
 
 @app.route('/todos', methods=['POST'])
-def add():
+def add_todo():
     data = request.json
     teacher = Todo(content=data['content'])
     db.session.add(teacher)
     db.session.commit()
     return 'Todo added'
+
+
+@app.route('/todos', methods=['GET'])
+def get_todos():
+    todos = Todo.query.all()
+    todos_json = [todo.to_json() for todo in todos]
+    return jsonify(todos_json)
