f173768e6719af745cf52d95b51f0a8a03065e0f
klima7 <ukasz.klimkiewicz2@wp.pl>
2021-11-10 17:28:56 +0100

    Basic database models created

--- back/app/models.py
+++ back/app/models.py
@@ -1,5 +1,4 @@
 from app import db
-import json
 
 
 class Todo(db.Model):
@@ -8,3 +7,30 @@ class Todo(db.Model):
 
     def to_json(self):
         return {"content": self.content}
+
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    email = db.Column(db.String(120), index=True, unique=True)
+    password_hash = db.Column(db.String(128))
+    boards = db.relationship("Board", backref="board", lazy='select')
+
+
+class Card(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(30))
+    description = db.Column(db.String)
+    column_id = db.Column(db.Integer, db.ForeignKey('column.id'))
+
+
+class Column(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    cards = db.relationship("Card", backref="card", lazy='select')
+    board_id = db.Column(db.Integer, db.ForeignKey('board.id'))
+
+
+class Board(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(40))
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+    columns = db.relationship("Column", backref="column", lazy='select')
