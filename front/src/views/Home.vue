840ba10a7bf9f2f16472b1d1fb519cb059cbcc9e
klima7 <ukasz.klimkiewicz2@wp.pl>
2021-11-17 21:38:19 +0100

    Home page created

--- front/src/views/Home.vue
+++ front/src/views/Home.vue
@@ -1,7 +1,19 @@
 <template>
-    <div>
-        <h1>Home</h1>
-    </div>
+    <v-container>
+        <v-row justify="center mt-6">
+            <v-img
+                max-height="300"
+                max-width="300"
+                src="https://avatars.slack-edge.com/2021-07-19/2282472048054_9a51d280179d828b3ad7_512.png"
+                ></v-img>
+        </v-row>
+        <v-row justify="center">
+            <h1>Welcome on Trello!</h1>
+        </v-row>
+        <v-row justify="center">
+            <h3>Better Trello</h3>
+        </v-row>
+    </v-container>
 </template>
 
 <script>
