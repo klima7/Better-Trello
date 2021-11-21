863c69a022def535080895d1755ba200bf392223
Radosław Pluta <222541@edu.p.lodz.pl>
2021-11-21 22:12:44 +0100

    Added board view

--- back/app/main/routes.py
+++ back/app/main/routes.py
@@ -1,6 +1,6 @@
 from . import main
 from .. import auth
-from flask import request, jsonify, abort
+from flask import json, request, jsonify, abort
 from app.models import *
 
 @main.route('/')
@@ -45,14 +45,44 @@ def add_board():
     if user is None:
         return abort(400, 'Login required')
     if 'name' not in request.json:
-        return abort(400, 'Table\'s name was not found')
+        return abort(400, '\'name\' field was not found in request')
     name = request.json['name']
     if len(name) < 1:
-        return abort(400, 'Table\'s name can not be blank')
+        return abort(400, 'Board\'s name can not be blank')
     if len(name) > 40:
-        return abort(400, 'Table\'s name can not exceed 40 characters')
+        return abort(400, 'Board\'s name can not exceed 40 characters')
     # Add new board db
     board = Board(name=name, user_id=user.id)
     db.session.add(board)
     db.session.commit()
     return {}, 200
+
+
+@main.route('/info', methods=['POST'])
+@auth.login_required
+def get_board_info():
+    user = auth.current_user()
+    # Verify arguments
+    if user is None:
+        return abort(400, 'Login required')
+    if 'id' not in request.json:
+        return abort(400, '\'id\' field was not found in request')
+    # Get board
+    board_id = request.json['id']
+    board_json = Board.query.filter_by(user_id=user.id, id=board_id).first().toJSON()
+    if board_json is None:
+        return abort(400, 'Board was not found')
+
+    # Output dict
+    columns_dict = []
+    columns = Column.query.filter_by(board_id=board_id).all()
+    for column in columns:
+        cards = Card.query.filter_by(column_id=column.id).all()
+        all_cards = []
+        for card in cards:
+            all_cards.append(card.toJSON())
+        columns_dict.append({'name': column.name, 'cards': all_cards})
+    if columns_dict is None:
+        return {}, 200
+    output_dict = {'board_name': board_json['name'], 'columns': columns_dict}
+    return jsonify(output_dict), 200
--- back/app/models.py
+++ back/app/models.py
@@ -2,14 +2,6 @@ from app import db
 from werkzeug.security import generate_password_hash, check_password_hash
 
 
-class Todo(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    content = db.Column(db.String)
-
-    def to_json(self):
-        return {"content": self.content}
-
-
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     email = db.Column(db.String(120), index=True, unique=True)
@@ -34,11 +26,19 @@ class Card(db.Model):
     description = db.Column(db.String)
     column_id = db.Column(db.Integer, db.ForeignKey('column.id'))
 
+    def toJSON(self):
+        return {"title": self.title, "description": self.description}
+
+
 
 class Column(db.Model):
     id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(40))
     cards = db.relationship("Card", backref="card", lazy='select')
     board_id = db.Column(db.Integer, db.ForeignKey('board.id'))
+
+    def toJSON(self):
+        return {"name": self.name}
 
 
 class Board(db.Model):
@@ -46,3 +46,6 @@ class Board(db.Model):
     name = db.Column(db.String(40))
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
     columns = db.relationship("Column", backref="column", lazy='select')
+
+    def toJSON(self):
+        return {"name": self.name}
