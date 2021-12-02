de213e6b97c3171ec6142c0d0bd5df88eac258f8
klima7 <ukasz.klimkiewicz2@wp.pl>
2021-12-02 02:50:22 +0100

    Explicit ids providing removed from db_sample_data Autoincrement is set on all primary keys and when record is added with explicit primary key value it goes crazy

--- back/app/db_sample_data.py
+++ back/app/db_sample_data.py
@@ -9,40 +9,41 @@ def add_sample_data():
     Column.query.delete()
     Board.query.delete()
 
-    # SAMPLE USER (password: testtest)
-    db.session.add(User(id=1, email='test@test.com', password="testtest"))
-
     # GROCERY LIST TEST BOARD
-    # CARDS
-    db.session.add(Card(id=1, order=0, title='Herbs & Spices', description='Basil, Oregano, Coriander, Cumin', column_id=1))
-    db.session.add(Card(id=2, order=0, title='Breakfast cereal', description='Porridge oats, Unsweetened gransola', column_id=2))
-    db.session.add(Card(id=3, order=1, title='Oily fish', description='Tuna, Salmon, Mackerel', column_id=2))
-    db.session.add(Card(id=4, order=2, title='Pulses', description='Red kidney beans, White beans, Green lentils', column_id=3))
-    # COLUMNS
-    db.session.add(Column(id=1, order=0, name='To buy', board_id=1))
-    db.session.add(Column(id=2, order=1, name='In chart', board_id=1))
-    db.session.add(Column(id=3, order=2, name='Bought', board_id=1))
-    # BOARDS
-    db.session.add(Board(id=1, name='Standard Grocery List', user_id=1))
+    card1 = Card(order=0, title='Herbs & Spices', description='Basil, Oregano, Coriander, Cumin')
+    card2 = Card(order=0, title='Breakfast cereal', description='Porridge oats, Unsweetened gransola')
+    card3 = Card(order=1, title='Oily fish', description='Tuna, Salmon, Mackerel')
+    card4 = Card(order=2, title='Pulses', description='Red kidney beans, White beans, Green lentils')
+
+    col1 = Column(order=0, name='To buy', cards=[card1])
+    col2 = Column(order=1, name='In chart', cards=[card2, card3])
+    col3 = Column(order=2, name='Bought', cards=[card4])
+
+    board1 = Board(name='Standard Grocery List', columns=[col1, col2, col3])
+
 
     # WORKOUT ROUTINE TEST BOARD
-    # CARDS
-    db.session.add(Card(id=5, order=0, title='Total body flow', description='Mat, resistance band', column_id=4))
-    db.session.add(Card(id=6, order=1, title='Cardio sculpt express', description='Mat, light hand weights', column_id=4))
-    db.session.add(Card(id=7, order=2, title='Ball workout', description='Mat and pilates/barre ball', column_id=4))
-    db.session.add(Card(id=8, order=0, title='Ballet glide & sculpt', description='Mat, chair, gliding disc (paper plates or rags)', column_id=5))
-    db.session.add(Card(id=9, order=1, title='Yoga fusion flow', description='Mat', column_id=5))
-    db.session.add(Card(id=10, order=2, title='Barre body blast', description='Mat, chair, light hand weights', column_id=5))
-    db.session.add(Card(id=11, order=3, title='Total body cardio sculpt', description='Mat, light hand weights', column_id=5))
-    db.session.add(Card(id=12, order=0, title='Upper + lower compound moves', description='Light hand weights', column_id=6))
-    db.session.add(Card(id=13, order=1, title='Total body mat workout', description='Mat', column_id=6))
-    db.session.add(Card(id=14, order=2, title='Quickfix total body workout', description='Mat', column_id=6))
-    db.session.add(Card(id=15, order=0, title='Quickfix ballet box', description='Light hand weights', column_id=7))
-    # COLUMNS
-    db.session.add(Column(id=4, order=0, name='New excercises', board_id=2))
-    db.session.add(Column(id=5, order=1, name='In current routine', board_id=2))
-    db.session.add(Column(id=6, order=2, name='Ideas for excercises', board_id=2))
-    db.session.add(Column(id=7, order=3, name='Past excercises', board_id=2))
-    # BOARDS
-    db.session.add(Board(id=2, name='Barlates Complete Workout List', user_id=1))
+    card5 = Card(order=0, title='Total body flow', description='Mat, resistance band')
+    card6 = Card(order=1, title='Cardio sculpt express', description='Mat, light hand weights')
+    card7 = Card(order=2, title='Ball workout', description='Mat and pilates/barre ball')
+    card8 = Card(order=0, title='Ballet glide & sculpt', description='Mat, chair, gliding disc (paper plates or rags)')
+    card9 = Card(order=1, title='Yoga fusion flow', description='Mat')
+    card10 = Card(order=2, title='Barre body blast', description='Mat, chair, light hand weights')
+    card11 = Card(order=3, title='Total body cardio sculpt', description='Mat, light hand weights')
+    card12 = Card(order=0, title='Upper + lower compound moves', description='Light hand weights')
+    card13 = Card(order=1, title='Total body mat workout', description='Mat')
+    card14 = Card(order=2, title='Quickfix total body workout', description='Mat')
+    card15 = Card(order=0, title='Quickfix ballet box', description='Light hand weights')
+
+    col4 = Column(order=0, name='New excercises', cards=[card5, card6, card7])
+    col5 = Column(order=1, name='In current routine', cards=[card8, card9, card10, card11])
+    col6 = Column(order=2, name='Ideas for excercises', cards=[card12, card13, card14])
+    col7 = Column(order=3, name='Past excercises', cards=[card15])
+
+    board2 = Board(name='Barlates Complete Workout List', columns=[col4, col5, col6, col7])
+
+    user = User(email='test@test.com', password="testtest", boards=[board1, board2])
+
+    # FINISH
+    db.session.add(user)
     db.session.commit()
